a87a4ca219aacadbc608a8ee58711987d16c7583
Samuel Forrest <samuel@samuelforrest.co.uk>
2026-03-21 16:12:39 +0000

    Update base URL in sitemap to sitstop.org

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://sitstop.co.uk";
+  const baseUrl = "https://sitstop.org";
 
   const staticPages: MetadataRoute.Sitemap = [
     {
